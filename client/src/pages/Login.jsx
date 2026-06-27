1f2ed51632843e9a32a5fae6f3d88c587382623f
Sujit Kshatriya <yogurt@Sujits-Air.lan>
2026-06-27 19:48:33 +0530

    navbar added to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
-
+import { Menu } from "lucide-react";
 
 function Login(){
     const navigate = useNavigate();
